aebe66b353e08bcb7afcb1558745e1354b29d5e6
Michael Olatunji <MichaelOlatunji.dev@gmail.com>
2024-08-10 16:09:09 +0100

    An attempt to fix the mobile navbar not working

--- static/js/index.js
+++ static/js/index.js
@@ -2,10 +2,21 @@ const hamburger = document.querySelector('.hamburger');
 const nav = document.querySelector('.nav-links');
 const closeIcon = document.querySelector('.close-icon');
 
-hamburger.addEventListener('click', () => {
+// Function to toggle navigation
+function toggleNav(event) {
+    event.preventDefault(); // Prevent any default behavior
     nav.classList.toggle('nav-active');
-});
+}
 
-closeIcon.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-})
+// Check if the device is touch-enabled
+const isTouchDevice = 'ontouchstart' in document.documentElement;
+
+if (isTouchDevice) {
+    // For touch devices
+    hamburger.addEventListener('touchstart', toggleNav);
+    closeIcon.addEventListener('touchstart', toggleNav);
+} else {
+    // For non-touch devices (desktop)
+    hamburger.addEventListener('click', toggleNav);
+    closeIcon.addEventListener('click', toggleNav);
+}
